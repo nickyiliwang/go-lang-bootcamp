c57cb921ef1c62edf7aa13924e7769de18ef901f
nickyiliwang <nickyiliwang@gmail.com>
2021-12-05 19:40:39 -0500

    go variable basics

---
+++ 1. Variables/1.1. Naming rules/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// Rules
+// 1. Variable names with capital letters are exported
+// 2. Variable name can start with underscore, ie. var _speed int
+// 3. Can't: start with number (3speed), start with punctuations (!speed, spe!ed)
+// *Punctuations have special meanings in go
+
+func main() {
+	// variabel declaration, name, static type
+	// go is strongly typed
+	var speed int
+
+	fmt.Println(speed)
+}
