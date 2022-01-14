f13a08c0bf0219449718272be46b991abaae53ed
nickyiliwang <nickyiliwang@gmail.com>
2022-01-14 15:52:54 -0500

    fallthrough statement

--- conditions/switch_fallthrough.go
+++ conditions/switch_fallthrough.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 // the fallthrough keyword inside of an switch statement invalidated the condition of the case below and just executes the next block of code
+// go automatically adds break statements unlike JS
+// fallthrough is rarely used
 func main() {
 	i := 125
 	switch {
@@ -14,6 +16,9 @@ func main() {
 		fallthrough
 	default:
 		fmt.Println("number")
-
 	}
 }
+
+// i = 125 OUTPUTS: "big" "positive" "number"
+// i = 99 OUTPUTS: "positive" "number"
+// i = -1 OUTPUTS: "number"
